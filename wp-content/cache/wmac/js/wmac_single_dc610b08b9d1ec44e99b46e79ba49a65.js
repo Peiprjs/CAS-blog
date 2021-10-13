fc6deeb6de946404c333ae7b4735ef325ec05527
Peipr <proca001@jvvgirona.eu>
2021-10-13 23:24:27 +0200

    WP2Static CREATE wp-content/cache/wmac/js/wmac_single_dc610b08b9d1ec44e99b46e79ba49a65.js

---
+++ wp-content/cache/wmac/js/wmac_single_dc610b08b9d1ec44e99b46e79ba49a65.js
@@ -0,0 +1,11 @@
+(function(){var container=document.querySelectorAll('.site-navigation');if(!container){return;}
+for(let i=0;i<container.length;i++){NanoSpaceMenu(container[i]);}})();function NanoSpaceMenu(container){var container,button,menu,links,i,len;menu=container.getElementsByTagName('ul')[0];if('undefined'===typeof menu){button.style.display='none';return;}
+if(-1===menu.className.indexOf('nav-menu')){menu.className+=' nav-menu';}
+links=menu.getElementsByTagName('a');for(i=0,len=links.length;i<len;i++){links[i].addEventListener('focus',toggleFocus,true);links[i].addEventListener('blur',toggleFocus,true);}
+function toggleFocus(){var self=this;while(-1===self.className.indexOf('nav-menu')){if('li'===self.tagName.toLowerCase()){if(-1!==self.className.indexOf('focus')){self.className=self.className.replace(' focus','');}else{self.className+=' focus';}}
+self=self.parentElement;}}
+(function(container){var touchStartFn,i,parentLink=container.querySelectorAll('.menu-item-has-children > a, .page_item_has_children > a');if('ontouchstart'in window){touchStartFn=function(e){var menuItem=this.parentNode,i;if(!menuItem.classList.contains('focus')){e.preventDefault();for(i=0;i<menuItem.parentNode.children.length;++i){if(menuItem===menuItem.parentNode.children[i]){continue;}
+menuItem.parentNode.children[i].classList.remove('focus');}
+menuItem.classList.add('focus');}else{menuItem.classList.remove('focus');}};for(i=0;i<parentLink.length;++i){parentLink[i].addEventListener('touchstart',touchStartFn,false);}}}(container));}
+(function(){document.addEventListener('keydown',function(event){var modal,selectors,elements,menuType,bottomMenu,activeEl,lastEl,firstEl,tabKey,shiftKey,selectors='input, a, button';if(document.body.classList.contains('nanospace-has-popup-active')){modal=document.querySelector('.nanospace-popup-active');elements=modal.querySelectorAll(selectors);elements=Array.prototype.slice.call(elements);lastEl=elements[elements.length-1];firstEl=elements[0];activeEl=document.activeElement;tabKey=event.keyCode===9;shiftKey=event.shiftKey;if(!shiftKey&&tabKey&&lastEl===activeEl){event.preventDefault();firstEl.focus();}
+if(shiftKey&&tabKey&&firstEl===activeEl){event.preventDefault();lastEl.focus();}}})})();
